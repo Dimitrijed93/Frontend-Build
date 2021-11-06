c1172af9147a30493054aa94ce098fec38dbefd5
Dimitrije Dragicevic <dimitrijed93@gmail.com>
2021-11-06 08:22:27 +0100

    init

---
+++ webpack.common.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })]
+}
